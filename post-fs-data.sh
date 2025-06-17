adc6054ad4697ba3e1c0584d76881aabc603bae9
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-06-17 23:56:39 +0300

    Create post-fs-data.sh

---
+++ post-fs-data.sh
@@ -0,0 +1,53 @@
+#!/system/bin/sh
+# (c) 2025 mrdoge0, Free Software Licensed under Apache-2.0
+
+# I don't know will anybody want this, but I get irritated from spoofed fingerprints.
+
+# Gather props.
+SYSTEMPROP="/system/build.prop"
+BRAND=$(grep -E 'ro.product.vendor.brand=' "$VENDORPROP" | cut -d'=' -f2)
+DEVICE=$(grep -E 'ro.product.vendor.device=' "$VENDORPROP" | cut -d'=' -f2)
+VER=$(grep -E 'ro.build.version.release_or_codename=' "$SYSTEMPROP" | cut -d'=' -f2)
+ID=$(grep -E 'ro.build.id=' "$SYSTEMPROP" | cut -d'=' -f2)
+INC=$(grep -E 'ro.build.version.incremental=' "$SYSTEMPROP" | cut -d'=' -f2)
+TYPE=$(grep -E 'ro.build.type=' "$SYSTEMPROP" | cut -d'=' -f2)
+TAGS=$(grep -E 'ro.build.tags=' "$SYSTEMPROP" | cut -d'=' -f2)
+
+# Generate more props.
+NAME="lineage_$DEVICE"
+FLAVOR="$NAME-$TYPE"
+
+# Generate ultimate props.
+FP="$BRAND/$NAME/$DEVICE:$VER/$ID/$INC:$TYPE/$TAGS"
+DESC="$FLAVOR $VER $ID $INC $TAGS"
+
+# Apply fingerprint props.
+resetprop -n ro.build.fingerprint "$FP"
+resetprop -n ro.system.build.fingerprint "$FP"
+resetprop -n ro.system_ext.build.fingerprint "$FP"
+resetprop -n ro.product.build.fingerprint "$FP"
+resetprop -n ro.odm.build.fingerprint "$FP"
+resetprop -n ro.vendor.build.fingerprint "$FP"
+resetprop -n ro.vendor_dlkm.build.fingerprint "$FP"
+
+# Apply description props.
+resetprop -n ro.build.description "$DESC"
+resetprop -n ro.system.build.description "$DESC"
+resetprop -n ro.system_ext.build.description "$DESC"
+resetprop -n ro.product.build.description "$DESC"
+resetprop -n ro.odm.build.description "$DESC"
+resetprop -n ro.vendor.build.description "$DESC"
+resetprop -n ro.vendor_dlkm.build.description "$DESC"
+
+# Apply product.name props.
+resetprop -n ro.product.name "$NAME"
+resetprop -n ro.product.system.name "$NAME"
+resetprop -n ro.product.product.name "$NAME"
+resetprop -n ro.product.system_ext.name "$NAME"
+resetprop -n ro.product.vendor.name "$NAME"
+
+# Apply ro.build.flavor prop.
+resetprop -n ro.build.flavor "$FLAVOR"
+
+# Exit.
+exit 0
